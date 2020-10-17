96bd77c50f7c6dc579ce19cc797f7742e3c12799
Aneena Fernandez <69071846+stepin105111@users.noreply.github.com>
2020-10-17 15:27:40 +0530

    Create Program2.cs

---
+++ Program2.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program2
+    {
+        static int len;
+        static void DiaplayArray(int[] arr)
+        {
+            Console.Write("Forward Array: ");
+            for (int i = 0; i < len; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+            Console.Write("Backward Array: ");
+            for (int i = len - 1; i >= 0; i--)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = new int[100];
+            Console.WriteLine("Enter the No.of Elements");
+            try
+            {
+                len = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the Elements");
+                for (int i = 0; i < len; i++)
+                {
+                    arr[i] = int.Parse(Console.ReadLine());
+                }
+
+                DiaplayArray(arr);
+            }
+            catch
+            {
+                Console.WriteLine("Invalid input");
+            }
+        }
+    }
+}
